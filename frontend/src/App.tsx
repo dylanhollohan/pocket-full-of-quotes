2cc8f7269da2198fae7791fa801394b3beb4edf9
dylanhollohan <dylan.hollohan@mail.utoronto.ca>
2023-02-19 16:37:11 -0500

    adds footer to app comp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import Navbar from './components/Navbar'
+import { Home, About } from './pages';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <header>
+          <Header/>
           <Navbar/>
         </header>
         <div className="pages">
@@ -16,8 +17,15 @@ const App = () => {
               path="/"
               element={<Home/>}
             />
+            <Route
+              path="/about"
+              element={<About/>}
+            />
           </Routes>
         </div>
+        <footer className='footer'>
+          Created by Dylan Hollohan, 2023
+        </footer>
       </BrowserRouter>
     </div>
   );
