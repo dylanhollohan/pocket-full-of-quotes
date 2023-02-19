3c3ad70a7b964b6d8fc90abe0e8c8370f52764b5
dylanhollohan <dylan.hollohan@mail.utoronto.ca>
2023-02-19 16:36:57 -0500

    adds css resolution in webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -8,14 +8,26 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: [/node_modules/],
         include: [path.resolve(__dirname, 'src')],
         use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: ['style-loader', 'css-loader'], //css loader runs first, webpack uses them in reverse.
       },
       {
         test: /\.tsx?$/,
         include: [path.resolve(__dirname, 'src')],
         use: 'ts-loader',
+      },
+      {
+        test: /\.png$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
       },
     ],
   },
