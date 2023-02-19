bcb38b284116d4aa7fffa3ed3694491ada28feed
dylanhollohan <dylan.hollohan@mail.utoronto.ca>
2023-02-19 16:39:10 -0500

    adds text to about page

---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,9 @@
+export const About: React.FC = () => {
+    return (
+        <div id="about">
+            <div>The app is a demonstration of my love of reading (and listening to audiobooks, I suppose).</div>
+            <div>It was and continues to be created with great care, and of course Node.js, React, Redux, redux-thunk, and MongoDB/Mongoose.</div>
+            <div>I put a lot of love into it, and so, I Dockerized it and deployed it to Google Cloud Platform with Cloud Build, Cloud Deploy and Cloud Run.</div>
+        </div>
+    )
+};
