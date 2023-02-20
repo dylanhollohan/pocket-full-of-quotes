90905f42dc0f18ba0feefa903aeddd50f0481612
dylanhollohan <dylan.hollohan@mail.utoronto.ca>
2023-02-20 07:56:15 -0500

    adds logout button

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,31 @@
 import Aurelius from '../../public/aurelius.png';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Header: React.FC = (props) => {
   return (
     <div className="header">
       <img id="aurelius" src={Aurelius} alt='Aurelius Nav'/>
-      <div id="title">Pocket Full of Quotes</div>
-      <div id="quoteAndButton">
-        <div id="mainQuote">
-          "A classic book is a book that has never finished saying what it has to say"
-          <br></br>
-          - Italo Calvino
+      <span id="title">Pocket Full of Quotes</span>
+      <span id="quote-and-button">
+        <div className="main-quote">
+        "A classic book is a book that has never finished saying what it has to say"
         </div>
-        <button>Log out</button>
-      </div>
+        <span className="main-quote-author">- Italo Calvino</span>
+        <Tooltip 
+          title="Log out"
+          arrow
+          placement="left"
+          enterDelay={500}
+          enterNextDelay={500}
+        >
+          <Button variant='text' color="info">
+            <LogoutIcon htmlColor="#383939" fontSize="large"/>
+          </Button>
+        </Tooltip>
+        
+      </span>
     </div>
   )
 }
