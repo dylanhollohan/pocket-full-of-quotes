891a11d16cb14763fa64b9fad20dfae648ff6023
dylanhollohan <dylan.hollohan@mail.utoronto.ca>
2023-02-20 07:57:13 -0500

    adds todo for about page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,9 +1,12 @@
 export const About: React.FC = () => {
+    // TODO: include some images representing the technologies I used.
     return (
         <div id="about">
-            <div>The app is a demonstration of my love of reading (and listening to audiobooks, I suppose).</div>
-            <div>It was and continues to be created with great care, and of course Node.js, React, Redux, redux-thunk, and MongoDB/Mongoose.</div>
-            <div>I put a lot of love into it, and so, I Dockerized it and deployed it to Google Cloud Platform with Cloud Build, Cloud Deploy and Cloud Run.</div>
+            <div>
+                <div>The app is a demonstration of my love of reading... and listening to audiobooks, I suppose.</div>
+                <div>It was and continues to be created with great care, and of course Node.js, React, Redux, redux-thunk, and MongoDB/Mongoose.</div>
+                <div>I put a lot of love into it, and so, I Dockerized it and deployed it to Google Cloud Platform with Cloud Build, Cloud Deploy and Cloud Run.</div>
+            </div>
         </div>
     )
 };
