1e5ea12dc90e528eee4553155c545f7b783b593b
dylanhollohan <dylanhollohan@gmail.com>
2023-05-13 14:06:55 -0400

    adds user actions

---
+++ frontend/src/modules/users/constants.ts
@@ -0,0 +1,11 @@
+export enum LoginRequestStatus {
+  IDLE = "idle",
+  PENDING = "pending",
+  FAILURE = "failure"
+}
+
+export enum LogoutRequestStatus {
+  IDLE = "idle",
+  PENDING = "pending",
+  FAILURE = "failure"
+}
---
+++ frontend/src/modules/users/state/actions.ts
@@ -0,0 +1,3 @@
+import { userSlice } from './reducers';
+
+export const { loginRequest, loginSuccess, logoutRequest, logoutSuccess } = userSlice.actions
