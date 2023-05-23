cc9bac26b4cfe70f9bce664026f2f08d91a66c6f
dylanhollohan <dylanhollohan@gmail.com>
2023-05-22 21:37:37 -0400

    exports signup stuff

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -2,4 +2,5 @@ export { default as Navbar } from './Navbar';
 export { default as Header } from './Header';
 export { default as Quote } from './Quote';
 export { default as LoginForm } from './LoginForm';
+export { default as SignupForm } from './SignupForm';
 export { default as Footer } from './Footer';
