cbf2f3549d776050edd5aa35dff697023677fd1e
dylanhollohan <dylanhollohan@gmail.com>
2023-05-28 20:54:39 -0400

    imports cors and uses cookie-parser middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,9 @@
 import express, { Express, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 import { quotesRouter, authRouter } from './routes'
 config();
 
@@ -8,6 +11,8 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: 'http://localhost:8080'}))
 app.use('/api/quotes', quotesRouter);
 app.use('/api/auth', authRouter);
 
