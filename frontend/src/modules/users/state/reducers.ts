1a8bb9444922cb85c4ece3e5890e5e7952015c1d
dylanhollohan <dylanhollohan@gmail.com>
2023-05-31 21:48:55 -0400

    logged-in user state reducers

--- frontend/src/modules/users/state/reducers.ts
+++ frontend/src/modules/users/state/reducers.ts
@@ -1,9 +1,9 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import type { User, SignupError } from '../types';
+import type { SignupError, SignupSuccessPayload, LoginSuccessPayload } from '../types';
 import { LoginRequestStatus, LogoutRequestStatus, SignupRequestStatus } from '../constants';
 
 export interface UserState {
-  loggedInUser: User | null;
+  loggedInUser: String | null;
   loginRequestStatus: LoginRequestStatus;
   logoutRequestStatus: LogoutRequestStatus;
   signupRequestStatus: SignupRequestStatus;
@@ -25,24 +25,29 @@ export const userSlice = createSlice({
     signupRequest: state => {
       state.signupRequestStatus = SignupRequestStatus.PENDING;
     },
-    signupSuccess: state => {
+    signupSuccess: (state, action: PayloadAction<SignupSuccessPayload>) => {
       state.signupRequestStatus = SignupRequestStatus.SUCCESS;
+      state.loggedInUser = action.payload.userId
     },
     signupFail: (state, action: PayloadAction<SignupError>) => {
       state.signupRequestStatus = SignupRequestStatus.FAILURE;
       state.signupError = action.payload;
     },
+    resetSignup: state => {
+      state.signupRequestStatus = SignupRequestStatus.IDLE;
+      state.signupError = undefined;
+    },
     // need to reset signupRequestStatus after the user is redirected to login page, or some other trigger.
     loginRequest: state => {
       state.loginRequestStatus = LoginRequestStatus.PENDING;
     },
-    loginSuccess: (state, action: PayloadAction<User>) => {
+    loginSuccess: (state, action: PayloadAction<LoginSuccessPayload>) => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
       state.loginRequestStatus = LoginRequestStatus.IDLE
-      state.loggedInUser = action.payload;
+      state.loggedInUser = action.payload.userId;
     },
     logoutRequest: state => {
       state.logoutRequestStatus = LogoutRequestStatus.PENDING;
