ad97092065b194e4a7655910cb9c47023a4a1513
dylanhollohan <dylanhollohan@gmail.com>
2023-06-01 22:29:50 -0400

    adds login request side effect

--- frontend/src/modules/users/state/thunks.ts
+++ frontend/src/modules/users/state/thunks.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
-import { SignupRequestPayload, SignupSuccessPayload } from '../types';
-import { signupSuccess, signupFail } from './actions';
+import { SignupRequestPayload, LoginRequestPayload, SignupSuccessPayload, LoginSuccessPayload } from '../types';
+import { signupSuccess, signupFail, loginFail } from './actions';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:4000/api'
@@ -9,7 +9,7 @@ const axiosInstance = axios.create({
 // signupRequest is the "thunk action creator"
 export function signupRequest(payload: SignupRequestPayload) {
   // signupRequestThunk is the "thunk function"
-  return async function signupRequestThunk(dispatch, getState) {
+  return async function signupRequestThunk(dispatch) {
     try {
       // Note that Axios automatically serializes payload as JSON (stringify)
       const response: AxiosResponse<SignupSuccessPayload>  = await axiosInstance({
@@ -18,10 +18,9 @@ export function signupRequest(payload: SignupRequestPayload) {
         withCredentials: true,
         data: payload
       });
-      console.log(response);
+      console.log('signup response: ', response);
       // @ts-ignore
       if (response.data.userId) {
-
         // @ts-ignore
         dispatch(signupSuccess({ userId: response.data.userId}));
       } else {
@@ -31,6 +30,31 @@ export function signupRequest(payload: SignupRequestPayload) {
       }
     } catch (error) {
       dispatch(signupFail({ message: "theres an error"}))
+    }
+  }
+}
+
+export function loginRequest(payload: LoginRequestPayload) {
+  return async function loginRequestThunk(dispatch, getState) {
+    try {
+      const response: AxiosResponse<LoginSuccessPayload>  = await axiosInstance({
+        method: 'post',
+        url: '/auth/login',
+        withCredentials: true,
+        data: payload
+      });
+      console.log('login response: ', response);
+      // @ts-ignore
+      if (response.data.userId) {
+        // @ts-ignore
+        dispatch(loginSuccess({ userId: response.data.userId}));
+      } else {
+        // @ts-ignore
+        dispatch(loginFail(response.errors))
+        // use reducers to set error messages in global, and then clear them when a new signup request is made, or otherwise navigation is used.
+      }
+    } catch (error) {
+      dispatch(loginFail({ message: "theres an error"}))
     }
   }
 }
