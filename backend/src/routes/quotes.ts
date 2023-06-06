15efa51552e7bb8ffe1e1415ce737ce6dbfb16ef
dylanhollohan <dylanhollohan@gmail.com>
2023-06-05 21:13:24 -0400

    applies auth middleware to quotes routes

--- backend/src/routes/quotes.ts
+++ backend/src/routes/quotes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { quotesController as controller }  from '../controllers';
+import { requireAuth } from '../middleware';
 
 export const quotesRouter = express.Router();
 
-quotesRouter.get('/', controller.getQuotes);
-quotesRouter.get('/shuffle', controller.getRandomQuotes);
-quotesRouter.get('/:id', controller.getQuote);
-quotesRouter.post('/', controller.addQuote);
-quotesRouter.delete('/:id', controller.deleteQuote);
-quotesRouter.patch('/:id', controller.updateQuote);
+quotesRouter.get('/', requireAuth, controller.getQuotes);
+quotesRouter.get('/shuffle', requireAuth, controller.getRandomQuotes);
+quotesRouter.get('/:id', requireAuth, controller.getQuote);
+quotesRouter.post('/', requireAuth, controller.addQuote);
+quotesRouter.delete('/:id', requireAuth, controller.deleteQuote);
+quotesRouter.patch('/:id', requireAuth, controller.updateQuote);
