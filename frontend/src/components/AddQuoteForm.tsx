757729a3287448a4bfd70300d507d862b7e5dc92
dylanhollohan <dylanhollohan@gmail.com>
2023-06-13 21:01:29 -0400

    updated quotes type

--- frontend/src/components/AddQuoteForm.tsx
+++ frontend/src/components/AddQuoteForm.tsx
@@ -12,7 +12,11 @@ import { selectLoggedInUser } from '../modules/users/state/selectors';
 import { loginRequest } from '../modules/users/state';
 import './styles/LoginForm.css';
 
-const AddQuoteForm: React.FC = () => {
+type AddQuoteFormProps = {
+  closeModal: () => void;
+}
+
+const AddQuoteForm: React.FC<AddQuoteFormProps> = ({ closeModal }) => {
   const dispatch = useAppDispatch();
   const currentUser = useAppSelector(selectLoggedInUser);
   const [author, setAuthor] = useState<String>("");
@@ -32,35 +36,38 @@ const AddQuoteForm: React.FC = () => {
   } 
 
   const handleSubmit = () => {
-    dispatch(addQuoteRequest({
-      author,
-      content,
-      source
-    }));
+    console.log('submitted!')
+    // dispatch(addQuoteRequest({
+    //   author,
+    //   content,
+    //   source
+    // }));
   }
 
 
   return (
-    <Paper className="login-paper">
-      <h1 className="login-header">Log In</h1>
-      <FormControl className="login__input-wrapper">
-        <InputLabel htmlFor="email-input">Email address</InputLabel>
-        <Input id="email-input" type="email" required onChange={handleEmailChange}/>
+    <Paper className="add-quote-paper">
+      <h1 className="add-quote-header">Add a Quote</h1>
+      <FormControl className="add-quote__input-wrapper">
+        <InputLabel htmlFor="author-input">Email address</InputLabel>
+        <Input id="author-input" type="text" required onChange={handleAuthorChange}/>
       </FormControl>
       <div className="v-space"/>
-      <FormControl className="login__input-wrapper">
+      <FormControl className="add-quote__input-wrapper">
+        <InputLabel htmlFor="content-input">Password</InputLabel>
+        <Input id="content-input" required onChange={handleContentChange} type="text"/>
+      </FormControl>
+      <FormControl className="add-quote__input-wrapper">
         <InputLabel htmlFor="password-input">Password</InputLabel>
-        <Input id="password-input" required onChange={handlePasswordChange} type="password" placeholder="enter password"/>
+        <Input id="source-input" onChange={handleSourceChange} type="text" placeholder="Where did you hear/read the quote?"/>
       </FormControl>
       <div className="v-space"/>
-      <Box className="login-button-wrapper">
-        <Button variant="contained" onClick={handleLogin}>Log In</Button>
-        <Link to="/signup">
-          <Button variant="contained">Go to Sign Up page</Button>
-        </Link>
+      <Box className="add-quote-button-wrapper">
+        <Button variant="contained" onClick={handleSubmit}>Submit Quote</Button>
+        <Button variant="contained" onClick={closeModal}>Cancel</Button>
       </Box>
     </Paper>
   );
 }
 
-export default LoginForm;
+export default AddQuoteForm;
