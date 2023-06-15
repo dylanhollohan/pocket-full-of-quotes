5cbe89f75b436e5379e1ccc2fd185c7b464375e4
dylanhollohan <dylanhollohan@gmail.com>
2023-06-14 21:29:40 -0400

    uses TextField for content

--- frontend/src/components/AddQuoteForm.tsx
+++ frontend/src/components/AddQuoteForm.tsx
@@ -6,11 +6,12 @@ import InputLabel from '@mui/material/InputLabel';
 import Input from '@mui/material/Input';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 import { useAppDispatch, useAppSelector } from '../state/hooks';
 import { selectLoggedInUser } from '../modules/users/state/selectors';
 import { loginRequest } from '../modules/users/state';
-import './styles/LoginForm.css';
+import './styles/AddQuoteForm.css';
 
 type AddQuoteFormProps = {
   closeModal: () => void;
@@ -45,14 +46,11 @@ const AddQuoteForm: React.FC<AddQuoteFormProps> = ({ closeModal }) => {
   }
 
   const style = {
-    position: 'absolute', // as 'absolute'
+    position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: '600px',
-    borderRadius: '5px',
-    // boxShadow: 24,
-    p: 4,
   };
 
 
@@ -66,14 +64,13 @@ const AddQuoteForm: React.FC<AddQuoteFormProps> = ({ closeModal }) => {
         </FormControl>
         <div className="v-space"/>
         <FormControl className="add-quote__input-wrapper">
-          <InputLabel htmlFor="content-input">Content</InputLabel>
-          <Input id="content-input"  multiline rows={4} required onChange={handleContentChange} type="text"/>
+          <TextField label="Content" multiline rows={4} required onChange={handleContentChange}/>
         </FormControl>
+        <div className="v-space"/>
         <FormControl className="add-quote__input-wrapper">
           <InputLabel htmlFor="password-input">Source</InputLabel>
           <Input id="source-input" onChange={handleSourceChange} type="text" placeholder="Where did you hear/read the quote?"/>
         </FormControl>
-        <div className="v-space"/>
         <Box className="add-quote-button-wrapper">
           <Button variant="contained" onClick={handleSubmit}>Submit Quote</Button>
           <Button variant="contained" onClick={closeModal}>Cancel</Button>
