b07dd26844a284b1b487acc4c50cfb932b8371a0
dylanhollohan <dylanhollohan@gmail.com>
2023-06-17 22:27:20 -0400

    adds wip quote-related reducers

--- frontend/src/modules/quotes/state/reducers.ts
+++ frontend/src/modules/quotes/state/reducers.ts
@@ -1,24 +1,47 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import type { Quote } from '../types';
-// import { LoginRequestStatus, LogoutRequestStatus } from '../constants';
+import type { AddQuoteSuccessPayload, getQuotesSuccessPayload } from '../types';
+import { RequestStatus } from '../../constants';
 
 export interface QuoteState {
-  selectedQuote: Quote | null;
-  quotes: Quote[] | null;
+  selectedQuote: AddQuoteSuccessPayload | null;
+  addQuoteRequestStatus: RequestStatus;
+  getQuotesRequestStatus: RequestStatus;
+  quotes: AddQuoteSuccessPayload[];
 }
 
 // Define the initial state using that type
 const initialState: QuoteState = {
   selectedQuote: null,
-  quotes: null,
+  addQuoteRequestStatus: RequestStatus.IDLE,
+  getQuotesRequestStatus: RequestStatus.IDLE,
+  quotes: [],
 }
 
 export const quoteSlice = createSlice({
   name: 'quote',
   initialState,
   reducers: {
-    setSelectedQuote: (state, action: PayloadAction<Quote>) => {
+    setSelectedQuote: (state, action: PayloadAction<AddQuoteSuccessPayload>) => {
       state.selectedQuote = action.payload;
+    },
+    addQuoteRequest: state => {
+      state.addQuoteRequestStatus = RequestStatus.PENDING;
+    },
+    addQuoteSuccess: (state, action: PayloadAction<AddQuoteSuccessPayload>) => {
+      state.addQuoteRequestStatus = RequestStatus.SUCCESS;
+    },
+    addQuoteFail: state => {
+      state.addQuoteRequestStatus = RequestStatus.FAILURE;
+    },
+    getQuotesRequest: state => {
+      state.getQuotesRequestStatus = RequestStatus.PENDING;
+    },
+    getQuotesSuccess: (state, action: PayloadAction<getQuotesSuccessPayload>) => {
+      state.getQuotesRequestStatus = RequestStatus.SUCCESS;
+      state.quotes = action.payload.quotes;
+    },
+    getQuotesFail: state => {
+      state.getQuotesRequestStatus = RequestStatus.FAILURE;
     }
   }
 })
