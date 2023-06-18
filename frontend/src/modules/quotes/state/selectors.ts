8bbfee58496b8d19224dc26a37011ae5eb6baec1
dylanhollohan <dylanhollohan@gmail.com>
2023-06-17 22:26:54 -0400

    selectors for add quote status, selected quotes

--- frontend/src/modules/quotes/state/selectors.ts
+++ frontend/src/modules/quotes/state/selectors.ts
@@ -1,3 +1,7 @@
+import { Root } from 'react-dom/client';
 import { RootState } from '../../../state/store';
 
 export const selectSelectedQuote = (state: RootState) => state.quotes.selectedQuote;
+export const selectAddQuoteRequestStatus = (state: RootState) => state.quotes.addQuoteRequestStatus;
+// export const selectGetQuotesRequestStatus = (state: RootState) => state.quotes.getQuotesRequestStatus;
+export const selectQuotes = (state: RootState) => state.quotes.quotes;
