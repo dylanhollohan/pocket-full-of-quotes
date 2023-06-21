15461e6de8a8f91f89a877131d2ffd36def0d931
dylanhollohan <dylanhollohan@gmail.com>
2023-06-21 08:18:01 -0400

    adds the added quote to state

--- frontend/src/modules/quotes/state/selectors.ts
+++ frontend/src/modules/quotes/state/selectors.ts
@@ -4,3 +4,4 @@ export const selectSelectedQuote = (state: RootState) => state.quotes.selectedQu
 export const selectAddQuoteRequestStatus = (state: RootState) => state.quotes.addQuoteRequestStatus;
 // export const selectGetQuotesRequestStatus = (state: RootState) => state.quotes.getQuotesRequestStatus;
 export const selectQuotes = (state: RootState) => state.quotes.quotes;
+export const selectAddedQuote = (state: RootState) => state.quotes.addedQuote;
