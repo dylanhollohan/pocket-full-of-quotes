4f820aa43b7ff8d8a2c13da74666245ea2ae78c1
dylanhollohan <dylanhollohan@gmail.com>
2023-06-26 21:50:51 -0400

    adds delete and edit buttons with tooltips

--- frontend/src/components/QuoteToolbar.tsx
+++ frontend/src/components/QuoteToolbar.tsx
@@ -1,6 +1,8 @@
 import Tooltip from '@mui/material/Tooltip';
 import ShuffleOutlinedIcon from '@mui/icons-material/ShuffleOutlined';
 import MapsUgcOutlinedIcon from '@mui/icons-material/MapsUgcOutlined';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import Button from '@mui/material/Button';
 import { useAppDispatch, useAppSelector } from '../state/hooks';
 import { selectLoggedInUser } from '../modules/users/state/selectors';
@@ -21,7 +23,16 @@ export const QuoteToolbar: React.FC<QuoteToolbarProps> = ({
   const handleShuffle = () => { 
     if (currentUser) { 
         dispatch(getQuotesRequest({ userId: currentUser }));
-}};
+  }};
+
+  const handleEdit = () => { 
+    console.log("handle edit");
+  };
+
+  const handleDelete = () => { 
+    console.log("handle edit");
+  };
+
   return (
     <div className="quotes-buttons">
       <div className="quotes-button-wrapper">
@@ -45,6 +56,28 @@ export const QuoteToolbar: React.FC<QuoteToolbarProps> = ({
               >
               <Button variant="text" color="info">
                   <MapsUgcOutlinedIcon htmlColor="#383939" fontSize="large" onClick={openModal}/>
+              </Button>
+          </Tooltip>
+          <Tooltip 
+              title={<h3 className="quotes-tooltip">Edit Quote</h3>}
+              arrow
+              placement="right"
+              enterDelay={500}
+              enterNextDelay={500}
+              >
+              <Button variant="text" color="info">
+                  <EditOutlinedIcon htmlColor="#383939" fontSize="large" onClick={handleEdit}/>
+              </Button>
+          </Tooltip>
+          <Tooltip 
+              title={<h3 className="quotes-tooltip">Delete selected quote(s)</h3>}
+              arrow
+              placement="right"
+              enterDelay={500}
+              enterNextDelay={500}
+              >
+              <Button variant="text" color="info">
+                  <DeleteOutlinedIcon htmlColor="#383939" fontSize="large" onClick={handleDelete}/>
               </Button>
           </Tooltip>
       </div>
